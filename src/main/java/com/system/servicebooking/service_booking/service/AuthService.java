6ce7ce9db14c66fcf8b4a3501cf15a8dfa97903b
Swetha <mistii3004@gmail.com>
2026-02-01 23:16:44 +0530

    add profile profile when provider registered

--- src/main/java/com/system/servicebooking/service_booking/service/AuthService.java
+++ src/main/java/com/system/servicebooking/service_booking/service/AuthService.java
@@ -1,26 +1,31 @@
 package com.system.servicebooking.service_booking.service;
 
 import com.system.servicebooking.service_booking.config.PasswordConfig;
-import com.system.servicebooking.service_booking.dto.LoginRequestDTO;
-import com.system.servicebooking.service_booking.dto.LoginResponseDTO;
-import com.system.servicebooking.service_booking.dto.RegisterRequest;
-import com.system.servicebooking.service_booking.dto.UserResponseDTO;
+import com.system.servicebooking.service_booking.dto.*;
+import com.system.servicebooking.service_booking.enums.UsersRole;
 import com.system.servicebooking.service_booking.exception.InvalidBookingStateException;
 import com.system.servicebooking.service_booking.exception.UnauthorizedActionException;
 import com.system.servicebooking.service_booking.exception.UserAlreadyExistsException;
 import com.system.servicebooking.service_booking.mapper.BookingMapper;
 import com.system.servicebooking.service_booking.mapper.UserMapper;
+import com.system.servicebooking.service_booking.model.ProviderProfile;
 import com.system.servicebooking.service_booking.model.User;
+import com.system.servicebooking.service_booking.repository.ProviderProfileRepository;
 import com.system.servicebooking.service_booking.repository.UserRepository;
 import com.system.servicebooking.service_booking.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class AuthService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ProviderProfileRepository providerProfileRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -45,6 +50,16 @@ public class AuthService {
         User user=mapper.toEntity(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         User userResponse= userRepository.save(user);
+        if(user.getRole() == UsersRole.PROVIDER){
+            ProviderProfile profile=new ProviderProfile();
+            profile.setUserId(user.getId());
+            profile.setServicesOffered((new ArrayList<>()));
+            profile.setExperience(0);
+            profile.setRating(0.0);
+            profile.setAvailability("AVAILABLE");
+            providerProfileRepository.save(profile);
+
+        }
         return mapper.toResponseDTO(userResponse);
     }
 
